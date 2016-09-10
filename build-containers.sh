83bf4c78f010d79759be394470af6d26aa5443fb
Quirin Fischer <fischerq.de@gmail.com>
2016-09-10 02:18:54 +0100

    fixed dataset/userstats, added nginx to build

--- build-containers.sh
+++ build-containers.sh
@@ -4,9 +4,11 @@ docker build -t tembryo/wisdota-retrieve -f ../wisdota-retrieve/Dockerfile-deplo
 docker build -t tembryo/wisdota-download -f ../wisdota-download/Dockerfile-deploy ..
 docker build -t tembryo/wisdota-analysis -f ../wisdota-analysis/Dockerfile-deploy ..
 docker build -t tembryo/wisdota-crawl -f ../wisdota-crawl/Dockerfile-deploy ..
+docker build -t tembryo/wisdota-nginx -f ../wisdota-nginx/Dockerfile-deploy ..
 docker push tembryo/wisdota-scheduler
 docker push tembryo/wisdota-webserver
 docker push tembryo/wisdota-retrieve
 docker push tembryo/wisdota-download
 docker push tembryo/wisdota-analysis
 docker push tembryo/wisdota-crawl
+docker push tembryo/wisdota-nginx
---
+++ build-stats-container.sh
@@ -0,0 +1,2 @@
+sudo docker build -t tembryo/wisdota-user-stats -f ../wisdota-user-stats/Dockerfile-deploy ..
+sudo docker push tembryo/wisdota-user-stats
